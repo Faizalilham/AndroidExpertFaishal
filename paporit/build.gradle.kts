205a8526b3d8f79d12a62c81a8b51133fcf57ab0
Faizalilham <faizalfalakh19@gmail.com>
2024-03-17 15:26:03 +0700

    main

--- paporit/build.gradle.kts
+++ paporit/build.gradle.kts
@@ -19,13 +19,7 @@ android {
     }
 
     buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
+        
     }
     buildFeatures{
         viewBinding = true
